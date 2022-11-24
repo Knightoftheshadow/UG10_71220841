12af7232cd0eec4b99c536d3ed5ea565acf86f3b
Knightoftheshadow <87977920+Knightoftheshadow@users.noreply.github.com>
2022-11-24 13:15:19 +0700

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,14 @@
+print ("~~~~~~~~~~~~~~~~~~~~~~\('v')/~~~~~~~~~~~~~~~")
+print ("Program penghitung volume bangun ruang")
+print ("~~~~~~~~~~~~~~~~~~~~~~\('v')/~~~~~~~~~~~~~~~")
+print ("pilihlah salah satu bangun ruang yang ingin dihitung volumenya:")
+print (" 1. limas")
+print (" 2. bola")
+print (" 3. prism " )
+print (" 4. kerucut")
+
+pilihan = int(input('masukkan pilihan anda:'))
+ pilihan == 1:
+   panjang = float(input('Masukkan panjang sisi alas limas'))
+   tinggi = float(input('Masukkan tinggi limas'))
+   volume = f'volume limas tersebut adlah {2*panjang*tinggi}
